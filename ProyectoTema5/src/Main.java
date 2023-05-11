9484962860d8158213bb6f273bf7c75a3d365ed2
Bruno <brunogonzalez7314@gmail.com>
2023-05-10 22:06:18 -0300

    proyecto5

---
+++ ProyectoTema5/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static CocheCRUD cocheCrud = new CocheCRUDImpl();
+    public static void main(String[] args) {
+
+        cocheCrud.delete();
+        cocheCrud.findAll();
+        cocheCrud.save();
+    }
+}
